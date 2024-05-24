5e63629256d3b30511e6d4c9a1593f3cca7670ee
zahraa Meky <eng.alzahraa.meky@gmail.com>
2024-05-24 19:03:35 +0400

    Finish hero & our bread section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,35 @@
-import React from 'react'
+'use client';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import Image from 'next/image';
 
 const Hero = () => {
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 500,
+      easing: 'ease-in-sine',
+    });
+  }, []);
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section id="hero" className="flex flex-col lg:flex-row w-full justify-between items-start">
+      <div className="w-full lg:w-[50%]">
+        <Image src="/hero.jpg" width={800} height={500} alt="hero" className="bg-cover bg-center"/>
+      </div>
+      <div className="w-full lg:w-[50%] flex flex-col gap-10 p-20 justify-center items-center">
+        <Image src="/logo.png" data-aos="zoom-in" data-aos-delay="200" width={120} height={100} alt="logo" />
+        <h1 data-aos="zoom-in" data-aos-delay="400" className="text-5xl text-black text-center leading-[55px]">
+          Fresh breads <br/> handcrafted daily
+        </h1>
+        <p data-aos="fade-up" data-aos-delay="600" className="text-center text-xl text-slate-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nihil harum amet consequuntur facilis sint tempore iusto necessitatibus illum fuga.
+        </p>
+        <button  data-aos="fade-up" data-aos-delay="800" className="border-2 border-black px-14 py-4 rounded-full font-bold uppercase">read more</button>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
-
+export default Hero;
--- components/OurBread.tsx
+++ components/OurBread.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
+'use client';
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { ourbreads } from "@/export";
+import Image from 'next/image';
+const backrondImage = {
+  backgroundImage: 'url("/banner4.jpg")',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+};
+
+
 
 const OurBread = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 500,
+      easing: 'ease-in-sine',
+    });
+  }, []);
 
-export default OurBread
+  return (
+    <section
+      id="breads"
+      className="w-full flex flex-col  justify-center items-center py-20 px-20 h-fit gap-10"
+      style={backrondImage}
+    >
+      <h2 data-aos="zoom-in" className="text-[55px] text-white font-semibold">Our Breads</h2>
+      <div className="grid grid-cols-1 lg:grid-cols-4 items-center justify-between gap-6" data-aos="zoom-in" data-aos-delay="200">
+        {ourbreads.map((bread) => (
+          <div key={bread.title} className="p-8 bg-white  rounded-lg flex flex-col gap-10 justify-center items-center">
+            <Image src={bread.image} width={300} height={400} alt="hero" className="w-[300px] h-[200px] rounded-lg transform transition-transform hover:scale-110 duration-300 cursor-pointer"/>
+
+            <h3 className="text-[20px] font-semibold text-black">{bread.title}</h3>
+            <p className="text-center">{bread.desc}</p>
+            <button className="border-2 border-black px-8 py-2 rounded-full font-semibold uppercase text-sm transform transition-transform hover:scale-110 duration-300 cursor-pointer">{bread.label}</button>
+
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default OurBread;
